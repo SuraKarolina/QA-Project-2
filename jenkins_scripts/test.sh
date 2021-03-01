493bf9b521d71a61deff3a1e2eef08745eb585f5
SuraKarolina <karolina.sura@outlook.com>
2021-03-01 20:20:34 +0000

    Jenkins

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-
-# test servcie1
-cd ./service1
+pwd
+python3 -m venv venv
+. venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml 
+cd service1
+python3 -m pytest --cov application
+python3 -m pytest --cov application
+cd ../service2
+python3 -m pytest --cov application
+cd ../service3
+python3 -m pytest --cov application
+cd ../service4
+python3 -m pytest --cov application
 cd ..
-
-# test servcie2
-cd ./service2
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
-
-# test servcie3
-cd ./service3
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
-
-# test servcie4
-cd ./service4
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-cd ..
+deactivate
