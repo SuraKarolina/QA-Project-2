c430b656bde798663812c1fff367ad0ecd7175b8
SuraKarolina <karolina.sura@outlook.com>
2021-03-01 22:44:21 +0000

    jenkins

--- jenkins_scripts/deploy.sh
+++ jenkins_scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -i ~/.ssh/rsa_id manager-swarm << EOF
+ssh -i /home/jenkins/.ssh/rsa_id manager-swarm << EOF
 scp /home/karolina_sura/QA-Project-2/docker-compose.yaml /home/jenkins/
 export SECRET_KEY=${SC_KEY} 
 export DATABASE_URI=${DB_URI} 
