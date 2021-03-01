a9e92df049bb90c9728ae8a971bd7a5a9b9da992
SuraKarolina <karolina.sura@outlook.com>
2021-03-01 22:51:42 +0000

    jenkins

--- jenkins_scripts/deploy.sh
+++ jenkins_scripts/deploy.sh
@@ -1,4 +1,4 @@
-scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@manager-swarm:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/id_rsa docker-compose.yaml 
 ssh -i ~/.ssh/id_rsa jenkins@manager-swarm << EOF
     export DATABASE_URI=${DATABASE_URI}
     export AUTHOR=${AUTHOR}
